58d73b669cd535bf92e78a3a5d864db00a690f6a
syedfaizz <syedfaizz45@gmail.com>
2021-10-05 17:07:57 +0530

    [faisal] Add: welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAY_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
